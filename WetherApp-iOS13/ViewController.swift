fd8bb921842e6e216ba5cc3b162effdbcfd19a73
Dennis <nndenninn@gmail.com>
2020-03-10 21:38:42 +0300

    Initial Commit

---
+++ WetherApp-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WetherApp-iOS13
+//
+//  Created by Dennis Nesanoff on 10.03.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
